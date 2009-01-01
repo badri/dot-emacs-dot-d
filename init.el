371ed0989674a9a8308aa789a79aaa59018f237b
badri <badri.dilbert@gmail.com>
2009-01-01 16:20:19 +0530

    implemented backward kill word from Steve Yegge's "Effective emacs" item #3.

--- init.el
+++ init.el
@@ -106,3 +106,7 @@
 (setq frame-title-format "emacs [%b %*%+ %f]")
 (setq icon-title-format "emacs [%b]")
 
+; backward kill word.
+(global-set-key "\C-w" 'backward-kill-word)
+(global-set-key "\C-x\C-k" 'kill-region)
+(global-set-key "\C-c\C-k" 'kill-region)
