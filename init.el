b21ffe807bf294ce4a1115cbec9fa83837d6b704
197472 <197472@PC068365.cts.com>
2009-06-03 10:06:42 +0530

    moved cua mode to top

--- init.el
+++ init.el
@@ -5,7 +5,7 @@
 (scroll-bar-mode -1)
 (tool-bar-mode -1)
 (menu-bar-mode -1)
-
+(cua-mode t)
 (require 'ido)
 (ido-mode t)
 (require 'python)
@@ -25,24 +25,7 @@
 ;; toggle line numbers using "M-x ln"
 (require 'linum)
 
-;; to visualize tabs
 (require 'blank-mode)
-
-;; git mode
-(add-to-list 'load-path "~/.emacs.d/git-emacs")
-(load-library "git-emacs")
-
-
-
-;; org mode
-(add-to-list 'load-path "~/.emacs.d/org-mode/lisp")
- (require 'org-install)
- (add-to-list 'auto-mode-alist '("\\.org$" . org-mode))
- (define-key global-map "\C-cl" 'org-store-link)
- (define-key global-map "\C-ca" 'org-agenda)
- (setq org-log-done t)
-
-
 
 (defalias 'eb 'eval-buffer)
 (defalias 'er 'eval-region)
@@ -72,3 +55,15 @@
 
 ; C-x rji will open init.el 
 (set-register ?i '(file . "~/.emacs.d/init.el"))
+(custom-set-variables
+  ;; custom-set-variables was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(inhibit-startup-screen t))
+(custom-set-faces
+  ;; custom-set-faces was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ )
