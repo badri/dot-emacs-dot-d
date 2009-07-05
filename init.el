1386785df3200d9646e613536f917f5f53e6cb47
Parthasarathy <parthaji@parthaji-desktop.(none)>
2009-07-05 18:51:17 +0530

    minimized init.el

--- init.el
+++ init.el
@@ -9,38 +9,11 @@
 (require 'ido)
 (ido-mode t)
 (require 'python)
-(load-library "badri-python")
+;;(load-library "badri-python")
 (load-library "arc")
 
-; blogging @ my wp ac.
-(require 'weblogger)
 
-(custom-set-variables
-  ;; custom-set-variables was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
- '(blank-chars (quote (tabs lines space-before-tab)))
- '(weblogger-config-alist (quote (("default" ("user" . "badrix") ("server-url" . "http://badrix.wordpress.com/xmlrpc.php") ("weblog" . "4167400"))))))
-(custom-set-faces
-  ;; custom-set-faces was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
- )
-; spell check on blog
-(autoload 'flyspell-mode "flyspell" "On-the-fly spelling checker." t)
-(require 'textile-minor-mode)
 
-(add-hook 'weblogger-entry-mode-hook 'textile-minor-mode)
-(add-hook 'weblogger-entry-mode-hook 'flyspell-mode)
-
-(defun publish-post ()
-  (interactive)
-  (textile-to-html-buffer-respect-weblogger)
-  (weblogger-publish-entry)
- )
-(global-set-key "\C-cbs" 'weblogger-start-entry)
 (cua-mode t)
 (load-library "badri-visual-nicities")
 (setq load-path (append (list (expand-file-name "~/.emacs.d/js2")) load-path))
@@ -50,8 +23,6 @@
 (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
 (load-library "js2")
 
-; arc stuff.
-(add-to-list 'auto-mode-alist '("\\.arc$" . arc-mode))
 
 (require 'pager)
 
@@ -60,34 +31,6 @@
 
 ; to visualize tabs
 (require 'blank-mode)
-
-; git mode
-(add-to-list 'load-path "~/.emacs.d/git-emacs")
-(load-library "git-emacs")
-
-; xml mode
-;path to where nxml is
-(set 'nxml-path "~/.emacs.d/nxml-mode/")
-(load (concat nxml-path "rng-auto.el"))
- (add-to-list 'auto-mode-alist
-              (cons (concat "\\." (regexp-opt '("xml" "xsd" "sch" "rng" "xslt" "svg" "rss") t) "\\'")
-                    'nxml-mode))
-  (unify-8859-on-decoding-mode)
-
-    (setq magic-mode-alist
-	  (cons '("<＼＼?xml " . nxml-mode)
-	  magic-mode-alist))
-   (fset 'xml-mode 'nxml-mode)
-
-
-; org mode
-(add-to-list 'load-path "~/.emacs.d/org-mode/lisp")
- (require 'org-install)
- (add-to-list 'auto-mode-alist '("\\.org$" . org-mode))
- (define-key global-map "\C-cl" 'org-store-link)
- (define-key global-map "\C-ca" 'org-agenda)
- (setq org-log-done t)
-
 
 
 (defalias 'eb 'eval-buffer)
@@ -118,6 +61,3 @@
 
 ; C-x rji will open init.el 
 (set-register ?i '(file . "~/.emacs.d/init.el"))
-
-; php mode 
-(require 'php-mode)
