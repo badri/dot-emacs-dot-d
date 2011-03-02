2d693e6ae3f7b29319dc21922c46f1b2faf7a951
badri <badri.dilbert@gmail.com>
2011-03-02 15:18:31 -0800

    autocomplete enabled, .profile file identified as php

--- init.el
+++ init.el
@@ -191,3 +191,18 @@
 (add-to-list 'auto-mode-alist '("\\.inc$" . php-mode))
 (add-to-list 'auto-mode-alist '("\\.install$" . php-mode))
 (add-to-list 'auto-mode-alist '("\\.module$" . php-mode))
+(add-to-list 'auto-mode-alist '("\\.profile$" . php-mode))
+
+
+(require 'auto-complete)
+(global-auto-complete-mode t)
+
+(define-key ac-complete-mode-map "\C-n" 'ac-next)
+(define-key ac-complete-mode-map "\C-p" 'ac-previous)
+
+(setq ac-auto-start nil)
+(global-set-key "\M-/" 'ac-start)
+(define-key ac-complete-mode-map "\M-/" 'ac-stop)
+
+(define-key ac-complete-mode-map "\t" 'ac-complete)
+(define-key ac-complete-mode-map "\r" nil)
