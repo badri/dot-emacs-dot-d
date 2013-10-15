05d2a54a06a1ad972d18dde01a5d73c937be6353
Lakshminp <badri.dilbert@gmail.com>
2013-10-15 12:21:09 +0530

    code folding for php files

--- init.el
+++ init.el
@@ -255,3 +255,12 @@
 ;; magit
 (require 'magit)
 (global-set-key (kbd "\C-c\C-g") 'magit-status)
+
+;; code folding
+(add-hook 'php-mode-hook
+  (lambda()
+    (local-set-key (kbd "C-c <right>") 'hs-show-block)
+    (local-set-key (kbd "C-c <left>")  'hs-hide-block)
+    (local-set-key (kbd "C-c <up>")    'hs-hide-all)
+    (local-set-key (kbd "C-c <down>")  'hs-show-all)
+    (hs-minor-mode t)))
