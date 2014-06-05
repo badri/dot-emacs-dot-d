c2bbf9826fd5e311c691a14148025dac2a702c3c
Badri <badri.dilbert@gmail.com>
2014-06-05 05:39:33 +0530

    submodule markdown removed

--- init.el
+++ init.el
@@ -201,8 +201,8 @@
 
 ;; cucumber mode
 (add-to-list 'load-path "~/.emacs.d/cucumber.el")
-(require 'feature-mode)
-(add-to-list 'auto-mode-alist '("\.feature$" . feature-mode))
+;;(require 'feature-mode)
+;; (add-to-list 'auto-mode-alist '("\.feature$" . feature-mode))
 
 ;; markdown
 (load-file "~/.emacs.d/markdown-mode/markdown-mode.el")
