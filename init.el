bb7638a7f2e2b5cc0db377eef23cf5fdc790cb3e
Lakshmi Narasimhan <badri.dilbert@gmail.com>
2014-07-15 23:10:21 +0530

    emmet mode

--- init.el
+++ init.el
@@ -293,3 +293,9 @@
     '("marmalade" .
       "http://marmalade-repo.org/packages/"))
 (package-initialize)
+
+;; emmet mode
+(require 'emmet-mode)
+
+(add-hook 'sgml-mode-hook 'emmet-mode) ;; Auto-start on any markup modes
+(add-hook 'css-mode-hook  'emmet-mode) ;; enable Emmet's css abbreviation.
