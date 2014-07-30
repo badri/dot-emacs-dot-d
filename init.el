437f8f99b6560132e56efef06699f8cd91f7ad6c
Lakshmi Narasimhan <badri.dilbert@gmail.com>
2014-07-30 22:50:30 +0530

    wrap html tag

--- init.el
+++ init.el
@@ -299,3 +299,22 @@
 
 (add-hook 'sgml-mode-hook 'emmet-mode) ;; Auto-start on any markup modes
 (add-hook 'css-mode-hook  'emmet-mode) ;; enable Emmet's css abbreviation.
+
+(defun wrap-html-tag (tagName)
+  "Add a tag to beginning and ending of current word or text selection."
+  (interactive "sEnter tag name: ")
+  (let (p1 p2 inputText)
+    (if (use-region-p)
+        (progn
+          (setq p1 (region-beginning) )
+          (setq p2 (region-end) )
+          )
+      (let ((bds (bounds-of-thing-at-point 'symbol)))
+        (setq p1 (car bds) )
+        (setq p2 (cdr bds) ) ) )
+
+    (goto-char p2)
+    (insert "</" tagName ">")
+    (goto-char p1)
+    (insert "<" tagName ">")
+    ))
